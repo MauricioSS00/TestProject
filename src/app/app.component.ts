4d17b984a70a9a24a3ba83ed7c13587608d7f78b
Maurício Elyan Souza da Silva <mauricioelyan00@gmail.com>
2022-02-16 23:10:50 -0300

    Finished Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
 import { Component } from '@angular/core';
 
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'TestProject';
+
+  questions: any[] = [
+    { id: 'RU', code: 1, type: 'string', title: 'Capitulo 1: Inicio', loop: false, init: true },
+    { id: 'RU', code: 1, type: 'string', title: 'Cidade', loop: false, init: false },
+    { id: 'RU', code: 2, type: 'string', title: 'Sexo', loop: false, init: false },
+    { id: 'RU', code: 2, type: 'string', title: 'Capitulo 2: Principal', loop: false, init: true },
+    {
+      id: 'RANK', code: 3, type: 'rank', title: 'Cite as 2 principais marcas', loop: false, init: false, options: [
+        { id: 1, label: 'Coca Cola' },
+        { id: 2, label: 'Fanta' },
+        { id: 3, label: 'Doly' },
+        { id: 4, label: 'Guarana' }
+      ]
+    },
+    { id: 'RM', code: 4, type: 'string', title: 'Quais gosta?', loop: true, init: false },
+    { id: 'RU', code: 5, type: 'number', title: 'Qual sua idade?', loop: false, init: false }
+  ];
+  questionsType = [
+    { label: 'Type 1' },
+    { label: 'Type 2' },
+    { label: 'Type 3' },
+    { label: 'Type 4' }
+  ];
+  structures = [
+    { label: 'Structure 1' },
+    { label: 'Structure 2' },
+    { label: 'Structure 3' },
+    { label: 'Structure 4' }
+  ];
+  interactionQuestions = [
+    { label: 'Interaction 1' },
+    { label: 'Interaction 2' },
+    { label: 'Interaction 3' },
+    { label: 'Interaction 4' }
+  ];
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.questions, event.previousIndex, event.currentIndex);
+  }
+
+  remove(question: any) {
+    this.questions.splice(this.questions.indexOf(question), 1);
+  }
+
+  removeOptionRank(questions: any[], option: any) {
+    questions.splice(questions.indexOf(option), 1);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { InputTextModule } from 'primeng/inputtext';
+import { DropdownModule } from 'primeng/dropdown';
+import { ButtonModule } from 'primeng/button';
+import { FieldsetModule } from 'primeng/fieldset';
+import { KeyFilterModule } from 'primeng/keyfilter';
+import { CheckboxModule } from 'primeng/checkbox';
+import { DataViewModule } from 'primeng/dataview';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +20,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
+    DragDropModule,
+    InputTextModule,
+    DropdownModule,
+    ButtonModule,
+    FieldsetModule,
+    KeyFilterModule,
+    CheckboxModule,
+    DataViewModule
   ],
   providers: [],
   bootstrap: [AppComponent]
